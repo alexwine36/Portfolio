060b9f46a024f02c26c5cbce534c7f2df54a72fa
Alex Wine <alexwine@awts.io>
2022-06-15 12:54:34 -0600

    fix(ui): updated galaxy background source

--- libs/ui/src/components/page-background/page-background.tsx
+++ libs/ui/src/components/page-background/page-background.tsx
@@ -28,7 +28,8 @@ background-color: transparent;
 // background-image: url(https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80);
 
 padding: ${theme.spacing(3)};
-background-image: url(${galaxyRes});
+background-image: url(backgrounds/galaxy.svg);
+// background-image: url(${galaxyRes});
 background-size: cover;
 background-position: center;
 `;
