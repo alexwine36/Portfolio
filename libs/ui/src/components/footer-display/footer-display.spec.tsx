213c7c70b36b15dd597460fd5cc9751fe4e490d6
Alex Wine <alexwine@awts.io>
2022-06-17 11:52:48 -0600

    feat(docs): added docs to the application with Docusaurus
    Contents are automatically generated with typedoc and placed in docusaurus docs folder

--- libs/ui/src/components/footer-display/footer-display.spec.tsx
+++ libs/ui/src/components/footer-display/footer-display.spec.tsx
@@ -1,10 +1,9 @@
 import { render } from '@testing-library/react';
-
-import FooterDisplay from './footer-display';
+import { FooterDisplay } from './footer-display';
 
 describe('FooterDisplay', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<FooterDisplay />);
+    const { baseElement } = render(<FooterDisplay links={[]} social={[]} />);
     expect(baseElement).toBeTruthy();
   });
 });
