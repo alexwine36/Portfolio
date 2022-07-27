0bea1ffc0e45ff4393f1849d12919f1ea95a7cd5
alexwine36 <alexwine36@gmail.com>
2022-07-27 14:02:17 -0600

    fix(gatsby): acronyms fixed and testing new vale review settings

--- apps/portfolio/gatsby-config.js
+++ apps/portfolio/gatsby-config.js
@@ -158,19 +158,6 @@ module.exports = {
 
         gatsbyRemarkPlugins: [
           {
-            resolve: 'gatsby-remark-acronyms',
-            options: {
-              acronyms: {
-                CRM: 'Customer Relationship Management',
-                ERP: 'Enterprise Resource Planning',
-                'CI/CD': 'Continuous Integration and Continuous Deployment',
-                CAD: 'Computer Aided Design',
-                ORM: 'Object Relational Mapping',
-                AWS: 'Amazon Web Services',
-              },
-            },
-          },
-          {
             resolve: 'gatsby-remark-autolink-headers',
           },
           {
@@ -208,6 +195,19 @@ module.exports = {
           },
           {
             resolve: 'gatsby-remark-numbered-footnotes',
+          },
+          {
+            resolve: 'gatsby-remark-acronyms',
+            options: {
+              acronyms: {
+                CRM: 'Customer Relationship Management',
+                ERP: 'Enterprise Resource Planning',
+                'CI/CD': 'Continuous Integration and Continuous Deployment',
+                CAD: 'Computer Aided Design',
+                ORM: 'Object Relational Mapping',
+                AWS: 'Amazon Web Services',
+              },
+            },
           },
         ],
       },
