95aa87961d6af6ff33e2c5b53ad4da59fd6a3e24
alexwine36 <alexwine36@gmail.com>
2022-08-12 15:34:10 -0600

    fix(test): updated failing tests

--- apps/portfolio/src/components/remark-link/index.spec.tsx
+++ apps/portfolio/src/components/remark-link/index.spec.tsx
@@ -1,10 +1,22 @@
 import { render } from '@testing-library/react';
-
+import * as Gatsby from 'gatsby';
+import React from 'react';
 import RemarkLink from './index';
+
+const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery');
+useStaticQuery.mockImplementation(() => ({
+  pages: {
+    nodes: [
+      {
+        path: '/sample/',
+      },
+    ],
+  },
+}));
 
 describe('RemarkLink', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<RemarkLink />);
+    const { baseElement } = render(<RemarkLink href="/sample/" />);
     expect(baseElement).toBeTruthy();
   });
 });
